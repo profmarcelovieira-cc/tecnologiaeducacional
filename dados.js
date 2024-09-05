d94380d3b07b5853de1b30f833622d09b88fa38e
profmarcelovieira-cc <profmarcelovieira.cc@gmail.com>
2024-09-05 11:59:36 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,97 @@
+let dados = [
+    {
+        titulo: "Simulador PHET",
+        descricao: "O vídeo apresenta o simulador Phet para ensinar eletrônica básica. O profº Marcelo Vieira demonstra circuitos simples de corrente contínua e alternada, explicando suas diferenças e como medi-las.",
+        link: "https://www.youtube.com/watch?v=dtuEaZd-P1U&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso"
+    },
+    {
+        titulo: "Introdução ao Tinkercad: Simulador de Circuitos Eletrônicos",
+        descricao: "O vídeo apresenta o simulador Tinkercad, uma ferramenta útil para ensinar eletrônica básica. O profº Marcelo Vieira demonstra circuitos simples de corrente contínua e alternada, explicando suas diferenças e como medi-las.",
+        link: "https://www.youtube.com/watch?v=PvwWWzzfxVo&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=2&pp=iAQB"
+    },
+    {
+        titulo: "Introdução ao Arduino: Atuadores",
+        descricao: "O profº Marcelo Vieira apresenta o Arduino, uma placa de prototipação, e demonstra como ligá-lo a um LED e a uma lâmpada. Ele também explica a diferença entre portas digitais e analógicas, e como usar o simulador Phet para simular circuitos elétricos.",
+        link: "https://www.youtube.com/watch?v=oeF3MX4ED5E&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=3&pp=iAQB"
+    },
+
+    {
+        titulo: "Arduino: Controlando Motores",
+        descricao: "O professor Marcelo Vieira demonstra como controlar diferentes tipos de motores utilizando o Arduino. Aborda conceitos como PWM para controlar a velocidade de motores DC e a utilização de drivers para motores de passo.",
+        link: "https://www.youtube.com/watch?v=V7q-jH8Qaus&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=5&pp=iAQB"
+    },
+    {
+        titulo: "Arduino: PWM",
+        descricao: "O professor Marcelo Vieira demonstra como utilizar um potenciômetro com o Arduino. Ele explica como ler o valor analógico do potenciômetro e como controlar a intensidade de um LED de acordo com o valor lido. O vídeo também aborda conceitos de mapeamento de valores.",
+        link: "https://www.youtube.com/watch?v=nKDE3dV81sM&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=4&pp=iAQB"
+    },
+    {
+        titulo: "Arduino: Motores",
+        descricao: "O professor Marcelo Vieira apresenta os motores no Arduino, explicando a diferença entre motores CC e motores de redução. Ele demonstra como ligar os motores e controlá-los usando o simulador Phet. Além disso, ele mostra como usar o motor de vibração para criar efeitos sonoros.",
+        link: " https://www.youtube.com/watch?v=V7q-jH8Qaus&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=5&pp=iAQB"
+    },
+      
+    {
+        titulo: "Arduino e Sensores: Lendo a Luz",
+        descricao: "O professor Marcelo Vieira demonstra como utilizar o sensor de luz com o Arduino. Ele explica a função do sensor, como conectá-lo à placa e como programar o Arduino para ler os valores de luminosidade. Além disso, o professor mostra como utilizar esses valores para controlar um LED, criando um circuito simples de acendimento automático.",
+        link: "https://www.youtube.com/watch?v=5mQ1CE3OtwI&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=6&pp=iAQB "
+    },
+    {
+        titulo: "Transformação de Energia mecânica em elétrica",
+        descricao: "O vídeo apresenta um projeto de feira de ciências que transforma energia mecânica em energia elétrica. O projeto consiste em um motor de corrente contínua com redução, 3 LEDs e uma casa forrada com papel alumínio. O projeto é simples e pode ser feito por alunos do 2º e 3º ano do ensino fundamental.",
+        link: " https://www.youtube.com/watch?v=ADJXYfZ4RIM"
+    },
+
+      {
+        titulo: "IA+Desenho de triângulos",
+        descricao: "O professor Marcelo Vieira apresenta uma aplicação de inteligência artificial que identifica triângulos equiláteros, isósceles e escalenos. A aplicação é útil para alunos que estão aprendendo figuras geométricas. O professor também discute a importância do planejamento e dos recursos pedagógicos na utilização da tecnologia em sala de aula.",
+        link: "https://youtu.be/scLhUx7tEFc?si=dg2ZTeje3189Gm9U "
+      },
+    {
+        titulo: "IoT na Sala de Aula: Mais Diversão e Aprendizado!",
+        descricao: "Este vídeo é sobre o uso de inteligência artificial (IA) e Internet das Coisas (IoT) na educação básica. O professor Marcelo Vieira demonstra como usar a IA para controlar uma lâmpada através do Scratch. Ele também discute as possibilidades de uso da IA e da IoT na educação básica, como a criação de jogos educativos e a personalização do ensino.",
+        link: "https://youtu.be/BtIGoLgosYE?si=U-jH-S21cyzyK5fW"
+    },
+      {
+        titulo: "IA na Educação: Ilumine a Aprendizagem!",
+        descricao: "Desenvolva atividades interdisciplinares com inteligência artificial. No exemplo podemos trabalhar figuras geométricas",
+        link: "https://www.youtube.com/watch?v=Tf7Y_WtorsI "
+      },
+      {
+        titulo: "HackEduca Arduino aula1: Introdução",
+        descricao: "Introdução e apresentação do programa HackEduca que será usado para uma série de vídeos educacionais como ambiente de programação para o Arduino",
+        link: " https://www.youtube.com/watch?v=lZcgLhuyJvs"
+      },
+      {
+        titulo: "HackEduca+Arduino- aula2: Liga/Desliga Led- Pisca",
+        descricao: "Breve apresentação do sistema BI BNCC que colabora no planejamento e elaboração dos planos de aula que utilizam tecnologia.",
+        link: " https://youtu.be/ngXzFY3dw_Y"
+      },
+      {
+        titulo: "HackEduca+Arduino- aula3: Sensor de luminosidade e controle do Led",
+        descricao: "Apresentação do LDR e criação de programa no HackEduca para controlar o acionamento do LED de forma automática.",
+        link: "https://www.youtube.com/watch?v=ypcu3rm9G-o "
+      },
+      {
+        titulo: "As vantagens e desafios da tecnologia na educação",
+        descricao: "Particpação no Portal Terra para falar sobre Tecnologia na Educação",
+        link: "https://www.youtube.com/watch?v=E8K7VGm-qM0 "
+      },
+
+      {
+        titulo: "Escolas superconectadas",
+        descricao: "Particpação no Podcast PodAprender Conectado",
+        link: "https://open.spotify.com/episode/1CQbh8GrJ3BnXYDGUv0IzK?si=vxupcmJMQ2m1LKKNKnAu8A&nd=1&dlsi=f354a83fc48a4b0d"
+      },   
+
+      
+      {
+        titulo: "Foco na BNCC e Habilidades Digitais",
+        descricao: "A BNCC impulsiona as habilidades digitais dos alunos! Nosso dashboard mostra como a Base Nacional Comum Curricular está sendo aplicada para desenvolver o pensamento computacional e a criatividade digital nas escolas.",
+        link: "https://lookerstudio.google.com/reporting/05b160b8-7bf5-4b51-ab55-9dfdebd48718/page/Smf7C?s=sGJEJEO3FYc"
+      },  
+
+
+      
+
+];
