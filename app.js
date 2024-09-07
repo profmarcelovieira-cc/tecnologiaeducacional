4b30d6804c96523209a6d1e6523f1ae652b1f06d
profmarcelovieira-cc <profmarcelovieira.cc@gmail.com>
2024-09-06 22:15:19 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,21 +2,46 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value
+
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Insira o texto para pesquisa<p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
-
-    // Itera sobre cada dado da lista de dados
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+        // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-        // Cria um novo elemento HTML para cada resultado
-        resultados += `
-            <div class="item-resultado">
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+                
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes (campoPesquisa)){
+            resultados += `
+            <div class="item-resultado center-video">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta">${dado.descricao}</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
+                <div class="video-container">
+                ${dado.iframe}
+                </div> 
             </div>
         `;
+        }    
+    }
+    if (!resultados){
+        resultados = "<p> Ainda não temos esse conteúdo na base de dados<p>";
+
+
     }
 
     // Atribui os resultados gerados à seção HTML
--- dados.js
+++ dados.js
@@ -2,96 +2,125 @@ let dados = [
     {
         titulo: "Simulador PHET",
         descricao: "O vídeo apresenta o simulador Phet para ensinar eletrônica básica. O profº Marcelo Vieira demonstra circuitos simples de corrente contínua e alternada, explicando suas diferenças e como medi-las.",
-        link: "https://www.youtube.com/watch?v=dtuEaZd-P1U&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso"
+        iframe: '<iframe src="https://www.youtube.com/embed/dtuEaZd-P1U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', 
+        link: "https://phet.colorado.edu/pt_BR/simulations/battery-resistor-circuit",
+        tags: "eletronica circuíto robótica robotica ensino fundamental Ensino fundamental",
     },
     {
         titulo: "Introdução ao Tinkercad: Simulador de Circuitos Eletrônicos",
         descricao: "O vídeo apresenta o simulador Tinkercad, uma ferramenta útil para ensinar eletrônica básica. O profº Marcelo Vieira demonstra circuitos simples de corrente contínua e alternada, explicando suas diferenças e como medi-las.",
-        link: "https://www.youtube.com/watch?v=PvwWWzzfxVo&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=2&pp=iAQB"
+        iframe: '<iframe src="https://www.youtube.com/embed/PvwWWzzfxVo?si=SI0hI-TggE14HyW8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link:"https://www.tinkercad.com/",
+        tags: "video robótica robotica ensino fundamental Ensino fundamental",
     },
     {
         titulo: "Introdução ao Arduino: Atuadores",
         descricao: "O profº Marcelo Vieira apresenta o Arduino, uma placa de prototipação, e demonstra como ligá-lo a um LED e a uma lâmpada. Ele também explica a diferença entre portas digitais e analógicas, e como usar o simulador Phet para simular circuitos elétricos.",
-        link: "https://www.youtube.com/watch?v=oeF3MX4ED5E&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=3&pp=iAQB"
+        iframe: '<iframe src="https://www.youtube.com/embed/oeF3MX4ED5E?si=YiBZZo4RBGD6Y5sw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.arduino.cc/",
+        tags: "arduino atuador motor Motor led lampada ensino fundamental Ensino fundamental",
     },
 
     {
         titulo: "Arduino: Controlando Motores",
         descricao: "O professor Marcelo Vieira demonstra como controlar diferentes tipos de motores utilizando o Arduino. Aborda conceitos como PWM para controlar a velocidade de motores DC e a utilização de drivers para motores de passo.",
-        link: "https://www.youtube.com/watch?v=V7q-jH8Qaus&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=5&pp=iAQB"
+        iframe:'<iframe src="https://www.youtube.com/embed/V7q-jH8Qaus?si=Aw8IbcVHR2eSgu65" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.arduino.cc/",
+        tags: "Motor de passo motor dc Motor ensino fundamental Ensino fundamental",
     },
     {
         titulo: "Arduino: PWM",
         descricao: "O professor Marcelo Vieira demonstra como utilizar um potenciômetro com o Arduino. Ele explica como ler o valor analógico do potenciômetro e como controlar a intensidade de um LED de acordo com o valor lido. O vídeo também aborda conceitos de mapeamento de valores.",
-        link: "https://www.youtube.com/watch?v=nKDE3dV81sM&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=4&pp=iAQB"
+        iframe: '<iframe src="https://www.youtube.com/embed/nKDE3dV81sM?si=xZdcR01R92khi3KP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.arduino.cc/",
+        tags: "potenciometro porta analógica analogica ensino fundamental Ensino fundamental",
     },
     {
         titulo: "Arduino: Motores",
         descricao: "O professor Marcelo Vieira apresenta os motores no Arduino, explicando a diferença entre motores CC e motores de redução. Ele demonstra como ligar os motores e controlá-los usando o simulador Phet. Além disso, ele mostra como usar o motor de vibração para criar efeitos sonoros.",
-        link: " https://www.youtube.com/watch?v=V7q-jH8Qaus&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=5&pp=iAQB"
+        inframe:'<iframe src="https://www.youtube.com/embed/V7q-jH8Qaus?si=Wit-MLVLPM9DawQq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.arduino.cc/",
+        tags: "Motor Redução reducao redução ensino fundamental Ensino fundamental",
     },
       
     {
         titulo: "Arduino e Sensores: Lendo a Luz",
         descricao: "O professor Marcelo Vieira demonstra como utilizar o sensor de luz com o Arduino. Ele explica a função do sensor, como conectá-lo à placa e como programar o Arduino para ler os valores de luminosidade. Além disso, o professor mostra como utilizar esses valores para controlar um LED, criando um circuito simples de acendimento automático.",
-        link: "https://www.youtube.com/watch?v=5mQ1CE3OtwI&list=PL04Mn_RV9IitD_kuBTmVC9_AxpsF27Dso&index=6&pp=iAQB "
+       iframe: '<iframe src="https://www.youtube.com/embed/5mQ1CE3OtwI?si=9XCdY07LjINqEq0k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+       link:"https://www.arduino.cc/",
+        tags:"sensores sensor Sensor LDR ldr funcao ensino fundamental Ensino fundamental",
     },
     {
         titulo: "Transformação de Energia mecânica em elétrica",
         descricao: "O vídeo apresenta um projeto de feira de ciências que transforma energia mecânica em energia elétrica. O projeto consiste em um motor de corrente contínua com redução, 3 LEDs e uma casa forrada com papel alumínio. O projeto é simples e pode ser feito por alunos do 2º e 3º ano do ensino fundamental.",
-        link: " https://www.youtube.com/watch?v=ADJXYfZ4RIM"
+        iframe: '<iframe src="https://www.youtube.com/embed/ADJXYfZ4RIM?si=DlposPVUCdT1avKN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://curriculointerativo.sedu.es.gov.br/odas/transformacao-de-energia-mecanica-em-eletrica-51697",
+        tags: "transformacao de energia eletrica eletricidade corrente ensino fundamental Ensino fundamental",
     },
 
       {
         titulo: "IA+Desenho de triângulos",
         descricao: "O professor Marcelo Vieira apresenta uma aplicação de inteligência artificial que identifica triângulos equiláteros, isósceles e escalenos. A aplicação é útil para alunos que estão aprendendo figuras geométricas. O professor também discute a importância do planejamento e dos recursos pedagógicos na utilização da tecnologia em sala de aula.",
-        link: "https://youtu.be/scLhUx7tEFc?si=dg2ZTeje3189Gm9U "
+        iframe: '<iframe src="https://www.youtube.com/embed/scLhUx7tEFc?si=cQgN8pZUys9gxokI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://teachablemachine.withgoogle.com/",
+        tags: "Inteligência artificial inteligencia Artificial Inteligencia Machine Learning machine learning ensino fundamental Ensino fundamental",
       },
     {
         titulo: "IoT na Sala de Aula: Mais Diversão e Aprendizado!",
         descricao: "Este vídeo é sobre o uso de inteligência artificial (IA) e Internet das Coisas (IoT) na educação básica. O professor Marcelo Vieira demonstra como usar a IA para controlar uma lâmpada através do Scratch. Ele também discute as possibilidades de uso da IA e da IoT na educação básica, como a criação de jogos educativos e a personalização do ensino.",
-        link: "https://youtu.be/BtIGoLgosYE?si=U-jH-S21cyzyK5fW"
+        iframe: '<iframe src="https://www.youtube.com/embed/BtIGoLgosYE?si=oh0Dua2613RGlUJK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.youtube.com/watch?v=cVHsdPXlOfg",
+        tags: "internet das coisas Internet Das Coisas iot ensino fundamental Ensino fundamental",
     },
       {
         titulo: "IA na Educação: Ilumine a Aprendizagem!",
         descricao: "Desenvolva atividades interdisciplinares com inteligência artificial. No exemplo podemos trabalhar figuras geométricas",
-        link: "https://www.youtube.com/watch?v=Tf7Y_WtorsI "
+        iframe: '<iframe src="https://www.youtube.com/embed/Tf7Y_WtorsI?si=ZaOnRw9lMfxqLZuc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://teachablemachine.withgoogle.com/",
+        tags: "Inteligência artificial inteligencia Artificial Inteligencia Machine Learning machine learning ensino fundamental Ensino fundamental",
       },
       {
         titulo: "HackEduca Arduino aula1: Introdução",
         descricao: "Introdução e apresentação do programa HackEduca que será usado para uma série de vídeos educacionais como ambiente de programação para o Arduino",
-        link: " https://www.youtube.com/watch?v=lZcgLhuyJvs"
+        iframe: '<iframe src="https://www.youtube.com/embed/lZcgLhuyJvs?si=lznkEjF0bOpoFaw9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.hackeduca.com.br/",
+        tags: "Robótica robótica robotica Robotica ensino fundamental Ensino fundamental",
       },
       {
         titulo: "HackEduca+Arduino- aula2: Liga/Desliga Led- Pisca",
         descricao: "Breve apresentação do sistema BI BNCC que colabora no planejamento e elaboração dos planos de aula que utilizam tecnologia.",
-        link: " https://youtu.be/ngXzFY3dw_Y"
+        iframe: '<iframe src="https://www.youtube.com/embed/ngXzFY3dw_Y?si=NRbWC0ORAJV5LuZI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link:"https://www.hackeduca.com.br/",
+        tags: "Robótica robótica robotica Robotica ensino fundamental Ensino fundamental",
       },
       {
         titulo: "HackEduca+Arduino- aula3: Sensor de luminosidade e controle do Led",
         descricao: "Apresentação do LDR e criação de programa no HackEduca para controlar o acionamento do LED de forma automática.",
-        link: "https://www.youtube.com/watch?v=ypcu3rm9G-o "
+        iframe: '<iframe src="https://www.youtube.com/embed/ypcu3rm9G-o?si=V8x8WztNS6G1Q5Ks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://www.hackeduca.com.br/",
+        tags: "Robótica robótica robotica Robotica ensino fundamental Ensino fundamental ldr",
       },
       {
         titulo: "As vantagens e desafios da tecnologia na educação",
         descricao: "Particpação no Portal Terra para falar sobre Tecnologia na Educação",
-        link: "https://www.youtube.com/watch?v=E8K7VGm-qM0 "
+        iframe:'<iframe src="https://www.youtube.com/embed/E8K7VGm-qM0?si=LwqdPi-Sv1wL4flE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        link: "https://youtu.be/k2GmmTj7tpY?si=sKDI2NRv04FbAF9F",
+        tags: "mídia midia entrevista Entrevista tecnologia educacional Educacional Tecnologia ensino fundamental Ensino fundamental",
       },
 
       {
         titulo: "Escolas superconectadas",
         descricao: "Particpação no Podcast PodAprender Conectado",
-        link: "https://open.spotify.com/episode/1CQbh8GrJ3BnXYDGUv0IzK?si=vxupcmJMQ2m1LKKNKnAu8A&nd=1&dlsi=f354a83fc48a4b0d"
+        iframe:'<iframe style="border-radius:12px" src="https://open.spotify.com/embed/episode/1CQbh8GrJ3BnXYDGUv0IzK?utm_source=generator"frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>',
+        link: "https://open.spotify.com/show/7FsFRPxwYcRTS6gfJYvG7g",
+        tags: "mídia midia entrevista Entrevista tecnologia educacional Educacional Tecnologia ensino fundamental Ensino fundamental",
       },   
-
       
       {
         titulo: "Foco na BNCC e Habilidades Digitais",
         descricao: "A BNCC impulsiona as habilidades digitais dos alunos! Nosso dashboard mostra como a Base Nacional Comum Curricular está sendo aplicada para desenvolver o pensamento computacional e a criatividade digital nas escolas.",
-        link: "https://lookerstudio.google.com/reporting/05b160b8-7bf5-4b51-ab55-9dfdebd48718/page/Smf7C?s=sGJEJEO3FYc"
-      },  
-
-
-      
-
+        iframe: "",
+        link: "https://lookerstudio.google.com/reporting/05b160b8-7bf5-4b51-ab55-9dfdebd48718/page/Smf7C?s=sGJEJEO3FYc",
+        tags: "bncc tecnologia educacional Tecnologia Educacional ensino fundamental Ensino fundamental",
+      },      
 ];
